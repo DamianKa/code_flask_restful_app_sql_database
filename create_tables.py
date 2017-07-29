c539a7ff4ed4d19cd75fd47cad71e0d3f1ee576d
DamianKa <damcash@gmail.com>
2017-07-29 14:43:29 +0100

    retrieving item resources from a database

--- create_tables.py
+++ create_tables.py
@@ -6,7 +6,10 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)" # id int; id INTEGER PRIMARY KEY - it will assign automathically number, we will need to assign only username and password
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)" # id int; id INTEGER PRIMARY KEY - it will assign automathically number, we will need to assign only username and password
+cursor.execute(create_table)
 
+cursor.execute("INSERT INTO items VALUES('test', 10.99)")
 
 connection.commit()
 connection.close()
