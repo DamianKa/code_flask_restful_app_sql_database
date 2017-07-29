091a76a6df2dde30111a05e9d2f18d0fac41d461
DamianKa <damcash@gmail.com>
2017-07-29 19:44:34 +0100

    adding put method

--- item.py
+++ item.py
@@ -59,14 +59,14 @@ class Item(Resource):
 
 
 
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
 
-    query = "INSERT INTO items VALUES (?, ?)"
-    cursor.execute(query, (item['name'], item['price']))
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
 
 
 
@@ -88,15 +88,43 @@ class Item(Resource):
     def put(self, name): # put is idempotent so I can call the funcktion many times and I will not end up with many items
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
 
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occured inserting the item"}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occured updating the item"}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
         return {'items': items}
